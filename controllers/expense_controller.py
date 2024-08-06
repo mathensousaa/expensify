5a51a56a45d19e2b206784d16caf745be9320a4b
Nicolas do Nascimento Ignacio <nicolas.n@aluno.ifsp.edu.br>
2024-08-06 11:00:25 -0300

    feat: user and expense controlers

--- controllers/expense_controller.py
+++ controllers/expense_controller.py
@@ -10,3 +10,45 @@ def add_expense(description, amount, date):
     db.refresh(expense)
     db.close()
     return expense
+
+def get_expenses_by_date(date):
+    db = SessionLocal()
+    expenses = db.query(Expense).filter(Expense.date == date).all()
+    db.close()
+    return expenses
+
+def update_expense(expense_id, description=None, amount=None, date=None):
+    db = SessionLocal()
+    expense = db.query(Expense).filter(Expense.id == expense_id).first()
+    if not expense:
+        return None
+
+    if description:
+        expense.description = description
+    if amount:
+        expense.amount = amount
+    if date:
+        expense.date = date
+    
+    db.commit()
+    db.refresh(expense)
+    db.close()
+    return expense
+
+def delete_expense(expense_id):
+    db = SessionLocal()
+    expense = db.query(Expense).filter(Expense.id == expense_id).first()
+    if not expense:
+        return None
+
+    db.delete(expense)
+    db.commit()
+    db.close()
+    return expense_id
+
+def get_all_expenses():
+    db = SessionLocal()
+    expenses = db.query(Expense).all()
+    db.close()
+    return expenses
+
--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -10,3 +10,50 @@ def create_user(username, email, hashed_password):
     db.refresh(user)
     db.close()
     return user
+
+def get_user_by_id(user_id):
+    db = SessionLocal()
+    user = db.query(User).filter(User.id == user_id).first()
+    db.close()
+    return user
+
+def update_user(user_id, username=None, email=None, hashed_password=None):
+    db = SessionLocal()
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        return None
+
+    if username:
+        user.username = username
+    if email:
+        user.email = email
+    if hashed_password:
+        user.hashed_password = hashed_password
+    
+    db.commit()
+    db.refresh(user)
+    db.close()
+    return user
+
+def delete_user(user_id):
+    db = SessionLocal()
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        return None
+
+    db.delete(user)
+    db.commit()
+    db.close()
+    return user_id
+
+def get_all_users():
+    db = SessionLocal()
+    users = db.query(User).all()
+    db.close()
+    return users
+
+def get_user_by_email(email):
+    db = SessionLocal()
+    user = db.query(User).filter(User.email == email).first()
+    db.close()
+    return user
